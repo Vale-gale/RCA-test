6ff7d4b74bad23e9277e2a860c46427dc4016f8f
Valentina Galeano <valentinagaleanow@gmail.com>
2023-08-04 07:40:59 -0500

    espanolviernes

---
+++ src/app/space/algo/page.tsx
@@ -0,0 +1,11 @@
+import SupportView from "src/sections/support/view/support-view";
+
+// ----------------------------------------------------------------------
+
+export const metadata = {
+  title: 'Design Masterclass Course',
+};
+
+export default function SupportPage() {
+  return <SupportView />;
+}
